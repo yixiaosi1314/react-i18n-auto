14938ec016ab5374315613765b0221cc12c2f21a
mr18 <zhou060806@163.com>
2020-05-23 14:53:42 +0800

    Update polyfill.js

--- src/generator/tplCode/polyfill.js
+++ src/generator/tplCode/polyfill.js
@@ -1,5 +1,5 @@
-window.LOCALE_VERSION = '${version}'
-window.LOCALE = {}
+window.$LOCALE_VERSION = '${version}'
+window.$LOCALE = {}
 window.$AI = function (key, val) {
   if (key && typeof val === 'string') { return window.LOCALE[key] || val }
   return val
